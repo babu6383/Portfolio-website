84bb4d43fcf23188f4c18f3940f486e2b2c2eec3
babu6383 <dineshbabu1748@gmail.com>
2024-09-08 13:43:32 +0530

    menu button updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,58 +1,108 @@
+import "./Navbar.css";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import menu_open from "../../assets/menu_open.svg";
+import menu_close from "../../assets/menu_close.svg";
 
-import './Navbar.css';
-// import logo from '../../assets/logo.svg'
-// import dinesh from '../../assets/dinesh.png'
-import AnchorLink from 'react-anchor-link-smooth-scroll';
-import menu_open from '../../assets/menu_open.svg'
-import menu_close from '../../assets/menu_close.svg'
-
-import { useEffect, useRef } from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // Import AOS styles
-
-
-
+import { useEffect, useState } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css"; // Import AOS styles
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // State to manage menu visibility
 
   useEffect(() => {
-    AOS.init({ duration: 3000 ,once :true});
+    AOS.init({ duration: 3000, once: true });
   }, []);
 
-  const menuRef = useRef();
-
-  const openMenu = () => {
-    if (menuRef.current) {
-      menuRef.current.style.right = "0"; // Show menu
-      AOS.refresh(); // Refresh AOS to account for changes
-    }
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen); // Toggle menu visibility
   };
-  
+
   const closeMenu = () => {
-    if (menuRef.current) {
-      menuRef.current.style.right = "-350px"; // Hide menu off-screen
-      AOS.refresh(); // Refresh AOS to account for changes
-    }
+    setIsMenuOpen(false); // Close menu
   };
-  
-  
+
   return (
-    <div className='navbar'>
-        <h1 className='dinesh'><span>D</span> inesh</h1>
-        <img src={menu_open} onClick={openMenu}  alt="" className='nav-mob-open' data-aos="fade-left"/>
-        <ul  ref={menuRef} className='nav-menu' role="menu" aria-hidden="true">
-          <img src={menu_close} onClick={closeMenu}    alt="Close menu" className="nav-mob-close" />
-          <li><AnchorLink className='anchor-link' href='#home' data-aos="fade-down"><p>Home</p></AnchorLink></li>
-          <li><AnchorLink className='anchor-link' offset={50} href='#about' data-aos="fade-down"><p>About</p></AnchorLink></li>
-          {/* <li><AnchorLink className='anchor-link' offset={50} href='#home'><p>Service</p></AnchorLink></li> */}
-          <li><AnchorLink className='anchor-link' offset={50} href='#work' data-aos="fade-down"><p>Portfolio</p></AnchorLink></li>
-          <li><AnchorLink className='anchor-link' offset={50} href='#condact' data-aos="fade-down"><p>Contact</p></AnchorLink></li>
-        </ul>
-        <div className="nav-connect"><AnchorLink className='anchor-link' offset={50} href='#condact' data-aos="fade-down"> Connect With Me</AnchorLink></div>
-
-
+    <div className="navbar">
+      <h1 className="dinesh">
+        <span>D</span> inesh
+      </h1>
+      <img
+        src={menu_open}
+        onClick={toggleMenu}
+        alt="Open menu"
+        className="nav-mob-open"
+        data-aos="fade-left"
+      />
+      <ul
+        className="nav-menu"
+        role="menu"
+        aria-hidden={!isMenuOpen}
+        style={{ right: isMenuOpen ? "0" : "-350px" }} // Conditional styling based on state
+      >
+        <img
+          src={menu_close}
+          onClick={closeMenu}
+          alt="Close menu"
+          className="nav-mob-close"
+        />
+        <li>
+          <AnchorLink
+            className="anchor-link"
+            href="#home"
+            onClick={closeMenu}
+            data-aos="fade-down"
+          >
+            <p>Home</p>
+          </AnchorLink>
+        </li>
+        <li>
+          <AnchorLink
+            className="anchor-link"
+            offset={50}
+            href="#about"
+            onClick={closeMenu}
+            data-aos="fade-down"
+          >
+            <p>About</p>
+          </AnchorLink>
+        </li>
+        <li>
+          <AnchorLink
+            className="anchor-link"
+            offset={50}
+            href="#work"
+            onClick={closeMenu}
+            data-aos="fade-down"
+          >
+            <p>Portfolio</p>
+          </AnchorLink>
+        </li>
+        <li>
+          <AnchorLink
+            className="anchor-link"
+            offset={50}
+            href="#condact"
+            onClick={closeMenu}
+            data-aos="fade-down"
+          >
+            <p>Contact</p>
+          </AnchorLink>
+        </li>
+      </ul>
+      <div className="nav-connect">
+        <AnchorLink
+          className="anchor-link"
+          offset={50}
+          href="#condact"
+          onClick={closeMenu}
+          data-aos="fade-down"
+        >
+          Connect With Me
+        </AnchorLink>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
