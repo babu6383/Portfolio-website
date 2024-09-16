57621d6dbc56a74cde2a8e242d9a2ab35770c011
babu6383 <dineshbabu1748@gmail.com>
2024-09-16 10:13:05 +0530

    projects updated

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,39 +1,47 @@
 const Services_Data = [
     {
+
         s_no:"01",
-        s_name:"Youtube-Clone",
-        s_desc:"Responsive,using youtube API in React...",
-        s_link: "https://main--myclonetube.netlify.app/"
+        s_name:"Food-del-App",
+        s_desc:"Fully Responsive Food-delivery App using React...",
+        s_link :"https://main--foodappinreact.netlify.app/"
+       
     },
     {
         s_no:"02",
-        s_name:"E-Commerce",
-        s_desc:"Fully Responsive E-commerce App using React..."
+        s_name:"Youtube-Clone",
+        s_desc:"Responsive,using youtube API in React...",
+        s_link: "https://main--myclonetube.netlify.app/"
+       
     },
     {
         s_no:"03",
-        s_name:"Image-Search App",
-        s_desc:"Image Search App Using Unspalsh Api...",
-        s_link: "https://main--imgsearchportal.netlify.app/"
+        s_name:"Portfolio Website",
+        s_desc:"Responsive Portfolio Using React...",
+        s_link: "https://selfsite.netlify.app/"
+       
     },
     {
         s_no:"04",
-        s_name:"Quote Generator",
-        s_desc:"Generate Random Quote using Api...",
-        s_link: "https://main--gotquotegenerator.netlify.app/"
+        s_name:"Image-Search App",
+        s_desc:"Image Search App Using Unspalsh Api...",
+        s_link: "https://main--imgsearchportal.netlify.app/"
+       
 
     },
     {
         s_no:"05",
-        s_name:"Password Generator",
-        s_desc:"Generate Random Passwords...",
-         s_link: "https://main--randpwdgenerator.netlify.app/"
+        s_name:"Quote Generator",
+        s_desc:"Generate Random Quote using Api...",
+        s_link: "https://main--gotquotegenerator.netlify.app/"
+       
     },
     {
         s_no:"06",
-        s_name:"Portfolio Website",
-        s_desc:"Responsive Portfolio Using React...",
-         s_link: "https://selfsite.netlify.app/"
+        s_name:"Password Generator",
+        s_desc:"Generate Random Passwords...",
+        s_link: "https://main--randpwdgenerator.netlify.app/"
+      
     },
 ]
  
